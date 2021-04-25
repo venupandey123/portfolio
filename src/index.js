b9c71268cdca4fd1f34f1d4ada5aba2a2258dfeb
Venu Gopal Pandey <62290901+venu1908@users.noreply.github.com>
2021-04-25 23:53:07 +0530

    oki

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "styled-components";
+import { theme } from "./themes/theme";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+     <App />
+  </ThemeProvider>
+  ,
   document.getElementById('root')
 );
 
